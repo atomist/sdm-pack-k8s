93af692484dacc3799a5be36f587a3345f60c3e5
David Dooling <david@atomist.com>
2019-01-25 16:11:28 -0600

    Always templatize specs
    Before we did not run provided specs through the template functions
    before patching.  This caused issues when patching service accounts
    and role bindings.  For convergence, evolution purposes it is probably
    a good idea to templatize the spec even when patching.
    
    Remove our nginx-ingress annotations from the default ingress spec.
    
    Fix apiVersion in deployment spec.
    
    Invert cluster role (binding)? conditionals in rbac.ts.

--- test/kubernetes/ingress.test.ts
+++ test/kubernetes/ingress.test.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2018 Atomist, Inc.
+ * Copyright © 2019 Atomist, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,11 +43,6 @@ describe("kubernetes/ingress", () => {
                 kind: "Ingress",
                 metadata: {
                     name: "cloudbusting",
-                    annotations: {
-                        "kubernetes.io/ingress.class": "nginx",
-                        "nginx.ingress.kubernetes.io/rewrite-target": "/",
-                        "nginx.ingress.kubernetes.io/client-body-buffer-size": "1m",
-                    },
                     labels: {
                         "app.kubernetes.io/managed-by": pv,
                         "app.kubernetes.io/name": r.name,
@@ -95,11 +90,6 @@ describe("kubernetes/ingress", () => {
                 apiVersion: "extensions/v1beta1",
                 kind: "Ingress",
                 metadata: {
-                    annotations: {
-                        "kubernetes.io/ingress.class": "nginx",
-                        "nginx.ingress.kubernetes.io/rewrite-target": "/",
-                        "nginx.ingress.kubernetes.io/client-body-buffer-size": "1m",
-                    },
                     labels: {
                         "app.kubernetes.io/managed-by": pv,
                         "app.kubernetes.io/name": r.name,
@@ -154,6 +144,7 @@ describe("kubernetes/ingress", () => {
                 ingressSpec: {
                     metadata: {
                         annotations: {
+                            "kubernetes.io/ingress.class": "nginx",
                             "nginx.ingress.kubernetes.io/client-body-buffer-size": "512k",
                             "nginx.ingress.kubernetes.io/limit-connections": "100",
                             "nginx.ingress.kubernetes.io/limit-rps": "25",
