6023305864073ca086b60505fea39c7b1f3e60c0
David Dooling <david@atomist.com>
2019-01-25 14:35:04 -0600

    Clean up dependencies and imports
    Move ts-essentials to dependencies since DeepPartial appears in
    exported types.
    
    @types/request is needed to avoid compilation error in
    @kubernetes/client-node.
    
    @types/retry is required to avoid compilation error in
    @atomist/sdm-core.

--- test/support/minikube.test.ts
+++ test/support/minikube.test.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2018 Atomist, Inc.
+ * Copyright © 2019 Atomist, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,9 +15,7 @@
  */
 
 import * as assert from "power-assert";
-import {
-    processMinikubeDockeEnv,
-} from "../../lib/support/minikube";
+import { processMinikubeDockeEnv } from "../../lib/support/minikube";
 
 describe("support/minikube", () => {
 
